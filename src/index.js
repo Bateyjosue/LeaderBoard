890b4ce4f8ed71685e814b554c540670af9025ff
Bateyjosue <josuebatey19@gmail.com>
2022-08-26 03:40:07 +0200

    Add fetch data and post data in API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import './style.css';
-import Score from './modules/gameScore.js';
+import { fetchData, postGames } from './modules/gameScore.js';
 
 const containerScore = document.querySelector('.scores');
-const Scores = new Score();
-Scores.collection.forEach((score) => {
-  containerScore.innerHTML += `
-    <li>${score.name}:${score.score}</li>
-  `;
+fetchData().then((data) => {
+  if (data.result.length !== 0) {
+    data.result.forEach((score) => {
+      containerScore.innerHTML += `
+        <li>${score.user}:${score.score}</li>
+      `;
+    });
+  } else {
+    containerScore.innerHTML = 'No Score Found';
+    containerScore.style.padding = '1rem';
+    containerScore.style.textAlign = 'center';
+  }
 });
 
 const form = document.querySelector('.board-new form');
@@ -15,9 +22,32 @@ form.innerHTML = `
     <input type="text" name="name" id="name" placeholder="Your Name" aria-label="Name">
   </div>
   <div class="field-input">
-    <input type="text" name="name" id="score" placeholder="Your Score" aria-label="Score">
+    <input type="text" name="score" id="score" placeholder="Your Score" aria-label="Score">
   </div>
   <div class="field-input btn">
-    <input type="submit" name="name" id="submit" value="Submit" aria-label="submit">
+    <input type="submit" name="submit" id="submit" value="Submit" aria-label="submit">
   </div>
 `;
+
+const submit = document.querySelector('.board-new form input[type="submit"]');
+const user = document.querySelector('.board-new form input[name="name"]');
+const score = document.querySelector('.board-new form input[name="score"]');
+const popup = document.querySelector('.popup');
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  postGames(user.value, score.value).then((res) => {
+    popup.style.visibility = 'visible';
+    popup.innerHTML = `
+      <span class="material-symbols-outlined"> task_alt </span>
+      <p>${res.result}</p>
+    `;
+    setTimeout(() => {
+      popup.style.visibility = 'hidden';
+    }, 3000);
+  });
+  user.value = '';
+  score.value = '';
+});
+
+const refresh = document.querySelector('.refresh-btn');
+refresh.addEventListener('click', () => window.location.reload());
